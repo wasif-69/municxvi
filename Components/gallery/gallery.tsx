438222631456b7d5d6414d0a2cc6eef229e577ad
wasif-69 <wasifkhalil69@gmail.com>
2026-07-04 15:06:23 +0500

    adding community and about us section

--- Components/gallery/gallery.tsx
+++ Components/gallery/gallery.tsx
@@ -18,7 +18,7 @@ const Gallery = () => {
     <section className="relative overflow-hidden bg-[#061222] py-28">
 
       {/* Background Glow */}
-      <div className="absolute left-1/2 top-1/2 h-128 w-lg -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#C9A227]/8 blur-[180px]" />
+      {/* <div className="absolute left-1/2 top-1/2 h-128 w-lg -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#C9A227]/8 blur-[180px]" /> */}
 
       <div className="relative z-10 mx-auto max-w-7xl px-6">
 
